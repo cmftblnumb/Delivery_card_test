e2edafa52b3972169852addd5668b22ad7ff840b
Kristina P <cmftblnumb@gmail.com>
2025-03-10 12:04:08 -0400

    corrected code, healdess

--- src/test/java/RegistrationClass.java
+++ src/test/java/RegistrationClass.java
@@ -1,37 +1,35 @@
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationClass {
-    @BeforeAll
-    static void setUp() {
-        Configuration.baseUrl = "https://localhost:9999";
-        Configuration.timeout = 15000;
-        Configuration.headless = true;
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    void shouldSubmitOrderFormSuccessfully() {
+    public void shouldSubmitOrderFormSuccessfully() {
         Selenide.open("http://localhost:9999");
 
-        String planningDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='city'] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
 
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue(planningDate);
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("+79991234567");
-        $("[data-test-id=agreement]").click();
-        $(".button__text").click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79161234567");
+        $("[data-test-id='agreement']").click();
+        $("[data-test-id='button'].button").click();
 
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Успешно!"));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 }
